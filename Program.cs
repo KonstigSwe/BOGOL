59cd0af3897c280165323f08a147f6529e66c405
KonstigSwe <swekonstig@gmail.com>
2020-10-16 09:54:04 +0200

    Modified thingys

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BOGOL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            bool online = true;
+            Console.WriteLine("Quit by writing quit");
+            while (online == true)
+            {
+
+
+                userInput = Console.ReadLine();
+                if (userInput != "quit")  // as long as the user dont write "quit"
+                {
+                    Console.WriteLine("command: {0}", userInput);
+                    Console.WriteLine();
+                }
+                if (userInput == "quit")  // Turns of pogram
+                {
+                    online = false;
+                    Console.WriteLine("Good Bye");
+                }
+            }
+
+
+        }
+    }
+}
